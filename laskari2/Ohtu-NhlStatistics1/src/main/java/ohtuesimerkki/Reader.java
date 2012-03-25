c1c254ca08b5c4c6da39e2f568b68cf5ffeec0d9
Noemj <Noemj@Noemj-PC.(none)>
2012-03-25 12:30:44 +0300

    Added NhlStatistics1

---
+++ laskari2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Reader.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ohtuesimerkki;
+
+import java.util.List;
+
+/**
+ *
+ * @author Noemj
+ */
+public interface Reader {
+    public List<Player> getPlayers();
+    public int extractInt(String str);
+
+    
+}
