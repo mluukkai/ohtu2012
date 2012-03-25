98d87632ffa17bde7677fd009b6414910ef5ba83
Juho Gävert <asdf@lol.com>
2012-03-25 21:50:24 +0300

    Meinas unohtua 2. ja 3. tehtävät. Tehe~

---
+++ laskari2/Ohtu-NhlStatistics1/src/main/java/ohtuesimerkki/Reader.java
@@ -0,0 +1,10 @@
+package ohtuesimerkki;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public interface Reader {
+    public List<Player> getPlayers();
+}
