10d9e4a81e9442498a9128223a507fc60de631e9
Anna Hietanen <anna.hietanen@helsinki.fi>
2012-03-25 21:48:23 +0300

    palaute ja lopullinen

--- laskari2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
+++ laskari2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Pankki.java
@@ -1,27 +1,14 @@
+﻿*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ohtu.verkkokauppa;
 
-class Pankki {
-
-    private static Pankki instanssi;
-
-    public static Pankki getInstance() {
-        if (instanssi == null) {
-            instanssi = new Pankki();
-        }
-
-        return instanssi;
-    }
-    private Kirjanpito kirjanpito;
-
-    public Pankki() {
-        kirjanpito = Kirjanpito.getInstance();
-    }
-
-    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
-        kirjanpito.lisaaTapahtuma("tilisiirto: tililtä " + tilille + " tilille " + tilille
-                + " viite " + viitenumero + " summa " + summa + "e");
-        
-        // täällä olisi koodi joka ottaa yhteyden pankin verkkorajapintaan
-        return true;
-    }
+/**
+ *
+ * @author Annurgaia
+ */
+public interface Pankki {
+    public boolean tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa);
+    
 }
--- laskari2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ laskari2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,55 +1,17 @@
+﻿/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ohtu.verkkokauppa;
 
-import java.util.*;
+/**
+ *
+ * @author Annurgaia
+ */
+public interface Varasto {
 
-public class Varasto {
-
-    private static Varasto instanssi;
-
-    public static Varasto getInstance() {
-        if (instanssi == null) {
-            instanssi = new Varasto();
-        }
-
-        return instanssi;
-    }
-    
-    private Kirjanpito kirjanpito;
-    private HashMap<Tuote, Integer> saldot;  
-    
-    public Varasto() {
-        kirjanpito = Kirjanpito.getInstance();
-        saldot = new HashMap<Tuote, Integer>();
-        alustaTuotteet();
-    }
-            
-    public Tuote haeTuote(int id){
-        for (Tuote t : saldot.keySet()) {
-            if ( t.getId()==id) return t;
-        }
-        
-        return null;
-    }
-
-    public int saldo(int id){
-        return saldot.get(haeTuote(id));
-    }
-    
-    public void otaVarastosta(Tuote t){        
-        saldot.put(t,  saldo(t.getId())-1 );
-        kirjanpito.lisaaTapahtuma("otettiin varastosta "+t);
-    }
-    
-    public void palautaVarastoon(Tuote t){
-        saldot.put(t,  saldo(t.getId())+1 );
-        kirjanpito.lisaaTapahtuma("palautettiin varastoon "+t);
-    }    
-    
-    private void alustaTuotteet() {
-        saldot.put(new Tuote(1, "Koff Portteri", 3), 100);
-        saldot.put(new Tuote(2, "Fink Bräu I", 1), 25);
-        saldot.put(new Tuote(3, "Sierra Nevada Pale Ale", 5), 30);
-        saldot.put(new Tuote(4, "Mikkeller not just another Wit", 7), 40);
-        saldot.put(new Tuote(5, "Weihenstephaner Hefeweisse", 4), 15);
-    }
+    public Tuote haeTuote(int id);
+    public int saldo(int id);
+    public void otaVarastosta(Tuote t);
+    public void palautaVarastoon(Tuote t);
 }
