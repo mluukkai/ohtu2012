69c20f9f683dc683c865625e04dbf009474b36c2
Noemj <Noemj@Noemj-PC.(none)>
2012-03-25 15:15:38 +0300

    Annotation

--- laskari2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
+++ laskari2/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Kauppa.java
@@ -1,5 +1,9 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Kauppa {
 
     private VarastoImpl varasto;
@@ -8,6 +12,7 @@ public class Kauppa {
     private ViitegeneraattoriImpl viitegeneraattori;
     private String kaupanTili;
 
+    @Autowired
     public Kauppa(VarastoImpl varasto, PankkiImpl pankki, ViitegeneraattoriImpl viitegeneraattori) {
         this.varasto = varasto;
         this.pankki = pankki;
